adad7074e85d0534901c7df0fd9c9563f3d54c6c
Andrey Tvorozhkov <tvorog@tvorog.me>
2018-06-14 15:39:34 +0300

    Add sass support, modules: true

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import styles from './styles/atoms/basics.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>Welcome to React</h1>
-        </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        console.log(styles.myCoolHeader);
+        return (
+            <div>
+                <header>
+                    <h1 className={styles.myCoolHeader}>Welcome to React</h1>
+                </header>
+                <p>
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
